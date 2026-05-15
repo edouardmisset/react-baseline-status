288001319ee68c98287dceb610ab7d21d57c4b54
Edouard Misset <edouardmisset@gmail.com>
2026-05-15 13:48:54 +0200

    chore: changed toolchain

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite-plus";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-})
+  fmt: {
+    ignorePatterns: [],
+  },
+  staged: {
+    "*": "vp check --fix",
+  },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  plugins: [react()],
+});
