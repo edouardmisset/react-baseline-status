6bd68783a96a1fc0383b232eb61448896fb2c7db
Edouard Misset <edouardmisset@gmail.com>
2026-05-17 04:47:08 +0200

    fix: update build target in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/react-baseline-status/",
+  build: {
+    target: "esnext",
+  },
   fmt: {
     ignorePatterns: [],
   },
